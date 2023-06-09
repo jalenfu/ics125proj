a69a3fbb66734cf7c7e13b40543756b5d7aa5c9c
ninjajfu <fu.jalen@gmail.com>
2023-06-08 19:24:18 -0700

    first attempt at live step count

--- frontend/App.js
+++ frontend/App.js
@@ -15,6 +15,8 @@ import { getAuth, signInWithEmailAndPassword, onAuthStateChanged } from "firebas
 import { getDatabase, ref, update } from "firebase/database";
 import { DeviceEventEmitter } from "react-native";
 
+import GoogleFit, {Scopes} from 'react-native-google-fit';
+
 const firebaseConfig = {
   apiKey: "AIzaSyCAPQLVsvywtLHZ9HnTYj3Zo035qW7V50o",
   authDomain: "ics125-2195d.firebaseapp.com",
@@ -36,6 +38,53 @@ function App() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [loggedIn, setLoggedIn] = useState(false);
+
+  var [dailySteps, setdailySteps] = useState(0);
+  var [heartRate, setHeartRate] = useState(0);
+  var [calories, setCalories] = useState(0);
+  var [hydration, setHydration] = useState(0);
+  var [sleep, setSleep] = useState(0);
+  var [weight, setWeight] = useState(0);
+  var [bloodPressure, setBloodPressure] = useState({});
+  var [loading, setLoading] = useState(true);
+
+  const options = {
+    scopes: [
+      Scopes.FITNESS_ACTIVITY_READ,
+      Scopes.FITNESS_ACTIVITY_WRITE,
+      Scopes.FITNESS_BODY_READ,
+      Scopes.FITNESS_BODY_WRITE,
+      Scopes.FITNESS_BLOOD_PRESSURE_READ,
+      Scopes.FITNESS_BLOOD_PRESSURE_WRITE,
+      Scopes.FITNESS_BLOOD_GLUCOSE_READ,
+      Scopes.FITNESS_BLOOD_GLUCOSE_WRITE,
+      Scopes.FITNESS_NUTRITION_WRITE,
+      Scopes.FITNESS_SLEEP_READ,
+    ],
+  };
+  
+  GoogleFit.checkIsAuthorized().then(() => {
+    var authorized = GoogleFit.isAuthorized;
+    console.log(authorized);
+    if (authorized) {
+      // if already authorized, fetch data
+    } else {
+      // Authentication if already not authorized for a particular device
+      GoogleFit.authorize(options)
+        .then(authResult => {
+          if (authResult.success) {
+            console.log('AUTH_SUCCESS');
+
+            // if successfully authorized, fetch data
+          } else {
+            console.log('AUTH_DENIED ' + authResult.message);
+          }
+        })
+        .catch(() => {
+          dispatch('AUTH_ERROR');
+        });
+    }
+  });
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
@@ -93,6 +142,8 @@ function App() {
       listener.remove();
     };
   }, []);
+
+  
 
   return (
     <NavigationContainer>
--- frontend/StepCountPage.js
+++ frontend/StepCountPage.js
@@ -1,21 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, DeviceEventEmitter } from 'react-native';
+import GoogleFit from 'react-native-google-fit';
 
 const StepCountPage = () => {
-  // Mock screen time data
-  const stepcountlast7days = [5025, 8070, 6234, 4182, 4302, 5102, 6302]; // Array of screen time values for the last 7 days
-  // const stepcounttoday = stepcountlast7days[6]+" steps";
+  const [stepcountlast24hours, setStepCountLast24Hours] = useState(0);
+  const [stepcountlast7days, setStepCountLast7Days] = useState([5025, 8070, 6234, 4182, 4302, 5102, 6302]);
   const totalStepCount = stepcountlast7days.reduce((sum, steps) => sum + steps, 0);
+  const [dailyStepCount, setDailyStepCount] = useState([0]);
 
-  DeviceEventEmitter.emit("OnSaveInfo", {
-    StepCount: stepcountlast7days
+  useEffect(() => {
+    const fetchData = async () => {
+      const today = new Date();
+      const last24HoursDate = new Date(today.getTime() - 24 * 60 * 60 * 1000);
+      
+      const options = {
+        startDate: last24HoursDate.toISOString(),
+        endDate: today.toISOString(),
+        bucketUnit: 'DAY',
+        bucketInterval: 1,
+      };
+
+      try {
+        const res = await GoogleFit.getDailyStepCountSamples(options);
+        if (res.length !== 0) {
+          for (let i = 0; i < res.length; i++) {
+            if (res[i].source === 'com.google.android.gms:estimated_steps') {
+              const data = res[i].steps.reverse();
+              setDailyStepCount(data[0].value);
+              break;
+            }
+          }
+        } else {
+          console.log('Not Found');
+        }
+      } catch (error) {
+        console.log('Error retrieving step count:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  DeviceEventEmitter.emit('OnSaveInfo', {
+    StepCount: stepcountlast7days,
   });
-  
-  
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Step Count Today</Text>
-      <Text style={styles.stepCount}>{stepcountlast7days[6]} steps</Text>
+      <Text style={styles.stepCount}>{dailyStepCount} steps</Text>
 
       <Text style={styles.title}>Step Count Last 7 Days</Text>
       <View style={styles.barGraph}>
@@ -44,7 +77,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginBottom: 10,
   },
-  screenTime: {
+  stepCount: {
     fontSize: 18,
     marginBottom: 20,
   },
@@ -55,6 +88,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#f2f2f2',
     paddingHorizontal: 10,
     paddingVertical: 20,
+  },
+  barContainer: {
+    alignItems: 'center',
   },
   bar: {
     width: 20,
