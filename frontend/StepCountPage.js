a5b0caded947ee94869f6f2ccd5dbc99d51cb0bb
EricL22 <38116692+EricL22@users.noreply.github.com>
2023-06-08 17:35:41 -0700

    Clean up Profile Page and Step Count Page for consistency

--- frontend/StepCountPage.js
+++ frontend/StepCountPage.js
@@ -2,24 +2,24 @@ import React from 'react';
 import { View, Text, StyleSheet, DeviceEventEmitter } from 'react-native';
 
 const StepCountPage = () => {
-  // Mock screen time data
-  const stepcountlast7days = [5025, 8070, 6234, 4182, 4302, 5102, 6302]; // Array of screen time values for the last 7 days
-  // const stepcounttoday = stepcountlast7days[6]+" steps";
-  const totalStepCount = stepcountlast7days.reduce((sum, steps) => sum + steps, 0);
+  // Mock step count data
+  const stepCountLast7Days = [5025, 8070, 6234, 4182, 4302, 5102, 6302]; // Array of step count values for the last 7 days
+  // const stepCountToday = stepCountLast7Days[6]+" steps";
+  const totalStepCount = stepCountLast7Days.reduce((sum, steps) => sum + steps, 0);
 
   DeviceEventEmitter.emit("OnSaveInfo", {
-    StepCount: stepcountlast7days
+    StepCount: stepCountLast7Days
   });
   
   
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Step Count Today</Text>
-      <Text style={styles.stepCount}>{stepcountlast7days[6]} steps</Text>
+      <Text style={styles.stepCount}>{stepCountLast7Days[6]} steps</Text>
 
       <Text style={styles.title}>Step Count Last 7 Days</Text>
       <View style={styles.barGraph}>
-        {stepcountlast7days.map((steps, index) => (
+        {stepCountLast7Days.map((steps, index) => (
           <View key={index} style={[styles.barContainer]}>
             <View
               style={[styles.bar, { height: (steps / totalStepCount) * 300 + '%' }]}
